176b2a73207f504d740d45d171e66f9def157910
sanajaveria97 <43511405+sanajaveria97@users.noreply.github.com>
2024-05-10 16:51:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+// const lego = document.querySelector(".lego");
+// window.addEventListener("scroll", function () {
+//   let offset1 = window.pageYOffset;
+//   console.log(offset1);
+//   lego.style.backgroundPositionY = offset1 * 1 + "px";
+// });
+
+// const parallax = document.querySelector(".rocket-class");
+// parallax.addEventListener("click", function () {
+//   let offset = window.pageYOffset;
+//   console.log(offset);
+//   const new_offset = offset * 0.9 + "px";
+//   console.log("new offset: " + new_offset);
+//   parallax.style.transform(translateY) = -180 + "px";
+// });
